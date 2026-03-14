be75531154b9e57eda8920f93ab6e20d036b564e
jimena hernandez <jimenahernandezg5@gmail.com>
2026-03-13 18:27:37 -0600

    Initial Commit

---
+++ RickandMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickandMortyApp
+//
+//  Created by Jimena Hernández García on 13/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
