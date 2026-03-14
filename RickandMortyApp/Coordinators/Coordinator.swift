1d172e347a992ec092636ce32327bfa7b401f7e0
jimena hernandez <jimenahernandezg5@gmail.com>
2026-03-13 18:49:51 -0600

    feat: add initial project structure

--- RickandMortyApp/Coordinators/Coordinator.swift
+++ RickandMortyApp/Coordinators/Coordinator.swift
@@ -4,4 +4,11 @@
 //
 //  Created by Jimena Hernández García on 12/03/26.
 //
+import UIKit
 
+protocol Coordinator: AnyObject {
+    var navigationController: UINavigationController { get set }
+    var childCoordinators: [Coordinator] { get set }
+    
+    func start()
+}
